7952ac1b286f80e4e82812a46c130bc386cccc8d
tttmyprojectcodes <techtalktitans368@gmail.com>
2026-07-04 00:48:20 +0530

    Update main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -4,6 +4,7 @@
 // Flutter App - Complete
 // =====================================================
 
+import 'package:esimnest/services/api_service.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
